1fa0c33ecf1a07e84a1ae4115e5bce9d2edf4270
marcelosf89 <masf89.rj@gmail.com>
2015-08-31 14:07:51 -0300

    Inclusao de Compra

---
+++ ContC.presentation.mvc/Controllers/ComprasController.cs
@@ -0,0 +1,27 @@
+﻿using ContC.presentation.mvc.Models.CompraModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContC.presentation.mvc.Controllers
+{
+    public class ComprasController : Controller
+    {
+        // GET: Compras
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Novo(int empresaId)
+        {
+            CompraNovoModel cnm = new CompraNovoModel();
+            cnm.EmpresaId = empresaId;
+
+            return View(cnm);
+        }
+
+    }
+}
---
+++ ContC.presentation.mvc/Models/CompraModels/CompraNovoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ContC.presentation.mvc.Models.CompraModels
+{
+    public class CompraNovoModel
+    {
+        public int EmpresaId { get; set; }
+        public int Id { get; set; }
+
+        public int FornecedorId { get; set; }
+        public int CategoriaId { get; set; }
+        public int QuntidadeVezes { get; set; }
+        public decimal Valor { get; set; }
+        public bool Pago { get; set; }
+
+        public IEnumerable<SelectListItem> TipoPagamentos { get; set; }
+        public IEnumerable<SelectListItem> Categorias { get; set; }
+        public IList<ProdutosModel> Pordutos { get; set; }
+    }
+}
